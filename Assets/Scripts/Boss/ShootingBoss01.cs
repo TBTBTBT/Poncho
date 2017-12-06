ef62ed922ca5a65e9db3d4fd7cb4de591809f2d0
田畑　翔伍 <shogo.tabata@sega.com>
2017-12-06 18:50:06 +0900

    挙動追加

--- Assets/Scripts/Boss/ShootingBoss01.cs
+++ Assets/Scripts/Boss/ShootingBoss01.cs
@@ -6,20 +6,46 @@ public class ShootingBoss01 : ShootingEnemyBase {
 	public GameObject bullet1;
 	public GameObject bullet2;
 	public GameObject bullet3;
+    TickEvent spanEv;
+    // Use this for initialization
+    protected override void Init() {
+        spanEv = new TickEvent(120);
+        spanEv.SetFunction(Shot01);
+    }
 
-	// Use this for initialization
-	void Init() {
-		
+    // Update is called once per frame
+    protected override void UpdateLate () {
+        spanEv.Invoke();
 	}
-	
-	// Update is called once per frame
-	void UpdateLate () {
-		
+	void Shot01(){
+        StartCoroutine("Attack02");
 	}
-	void Shot1(){
-		
+	IEnumerator Attack01(){
+        float radius = 360;
+        bool isAim = false;
+        int num = 20;
+        for (int i = 0;i < num; i++)
+        {
+            float rad = -(radius / 2) + ((radius / (float)num) / 2) + (float)i * (radius / (float)num);
+            GameObject go = Instantiate(bullet1, transform.position, Quaternion.identity);
+            go.GetComponent<ShootingBulletBase>().Set(rad, transform.position, isAim);
+            yield return new WaitForSeconds(0.02f);
+        }
 	}
-	IEnumerator Attack1(){
-		return null;
-	}
+    IEnumerator Attack02()
+    {
+        float radius = 360;
+        bool isAim = false;
+        int num = 20;
+        for (int i = 0; i < num; i++)
+        {
+
+            for (int j = 0; j <2; j++)
+            {
+                GameObject go = Instantiate(bullet1, transform.position, Quaternion.identity);
+                go.GetComponent<ShootingBulletBase>().Set(180, transform.position-new Vector3(0,0.5f - 1 * j,0), false);
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
 }
